054011ac6e6868106fc7950ce80a4333afec953b
FelippeCremona <ycremona@hotmail.com.br>
2023-10-10 17:34:11 -0300

    teste

--- Teste.java
+++ Teste.java
@@ -1,7 +1,11 @@
+import java.io.File;
+
 public class Teste {
 
   public static void main(String[] args) {
+    File file = new File("teste.txt");
     System.out.println("Hello World!");
+    file.delete();
   }
   
 }
